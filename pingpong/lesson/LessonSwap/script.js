ac91b12ec3a13ee0f72a94cd6553e9b80d96e35a
SungYongKang-tech <sungyong01@gmail.com>
2025-07-28 09:51:51 +0900

    Update script.js

--- pingpong/lesson/LessonSwap/script.js
+++ pingpong/lesson/LessonSwap/script.js
@@ -11,7 +11,7 @@ onValue(scheduleRef, (snapshot) => {
 });
 
   const initialData = {
-    mon_0: { name: "김승일" }, tue_0: { name: "정승목" }, wed_0: { name: "김승일" }, thu_0: { name: "정승목" },
+    mon_0: { name: "정승목" }, tue_0: { name: "김승일" }, wed_0: { name: "정승목" }, thu_0: { name: "김승일" },
     mon_1: { name: "이상준" }, tue_1: { name: "박나령" }, wed_1: { name: "이상준" }, thu_1: { name: "박나령" },
     mon_2: { name: "이낭주" }, tue_2: { name: "양충현" }, wed_2: { name: "이낭주" }, thu_2: { name: "양충현" },
     mon_3: { name: "조보미" }, tue_3: { name: "송은아" }, wed_3: { name: "조보미" }, thu_3: { name: "송은아" },
